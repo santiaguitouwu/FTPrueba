472da5dddc604875a76833e2201ae2edebc14644
Santiago Jimenez <santiago.abihatar.jimenez@correounivalle.edu.co>
2024-01-20 18:57:56 -0500

    Se agrega la conexion y consulta a BD

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -1,5 +1,22 @@
 package Controlador;
 
-public class Controlador {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.ServerErrorException;
+
+import java.io.IOException;
+
+public class Controlador extends HttpServlet {
+    protected void ProcessRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+            String accion = request.getParameter("accion");
+            switch (accion){
+                case "Principal":
+                    request.getRequestDispatcher("Principal.jsp").forward(request,response);
+                    break;
+            }
+    }
 
 }
--- src/main/java/Controlador/Validar.java
+++ src/main/java/Controlador/Validar.java
@@ -3,17 +3,18 @@ package Controlador;
 import Modelo.Profesor;
 import Modelo.ProfesorDAO;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.ws.rs.GET;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Validar extends HttpServlet {
     ProfesorDAO profesorDAO = new ProfesorDAO();
     Profesor profesor = new Profesor();
-
+    private static final Logger LOGGER = Logger.getLogger(Validar.class.getName());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("Estoy en el get");
@@ -21,16 +22,18 @@ public class Validar extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String accion = req.getParameter("accion");
-        if (accion.equals("Ingresa")){
+
+        if (accion.equals("Ingresar")){
             String user = req.getParameter("txtuser"); //se obtienen los datos de ingreso del usuario
             String pass = req.getParameter("txtpass");
             profesor = profesorDAO.validar(user,pass);
+            LOGGER.info("Usuario" + user + ", Contraseña" + pass);
             if (profesor.getID_Profesor()!=null){
-                req.getRequestDispatcher("Controlador?accion=Principal").forward(req,resp); //si el usuario existe se redirecciona al controlador
+                req.getRequestDispatcher("Controlador?accion=Principal.jsp").forward(req,resp); //si el usuario existe se redirecciona al controlador
             }else{
-                req.getRequestDispatcher("index.jso").forward(req,resp); //si no existe se direcciona a la misma página de login
+                req.getRequestDispatcher("index.jsp").forward(req,resp); //si no existe se direcciona a la misma página de login
             }
         }
-        else{req.getRequestDispatcher("index.jso").forward(req,resp);}//si se presiona algún otro botón retorna a la misma página
+        else{req.getRequestDispatcher("index.jsp").forward(req,resp);}//si se presiona algún otro botón retorna a la misma página
     }
 }
