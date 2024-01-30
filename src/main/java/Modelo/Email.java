7750e6428458d1e82fa3ecf35bbfdcbbac338c6f
Santiago Jimenez <santiago.abihatar.jimenez@correounivalle.edu.co>
2024-01-29 22:52:54 -0500

    Agregando funcionalidades a la clase Email

--- src/main/java/Modelo/Email.java
+++ src/main/java/Modelo/Email.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Email {
     String pais;
@@ -32,13 +34,30 @@ public class Email {
         listaEmails = email.readEmail();
         if (listaEmails.contains(nuevoEmail)){
             LOGGER.info("El email ya existe");
-            nuevoEmail = nombre + "." + apellido + "1@fasttrack.com." + dominio;
+            // Buscar el último número existente en los correos electrónicos con el mismo formato
+            String patronCorreoExistente = nombre + "." + apellido + "(\\d*)@fasttrack.com." + dominio;
+            Pattern patron = Pattern.compile(patronCorreoExistente);
+            int maxNumero = 0;
+            for (String correo : listaEmails) {
+                Matcher matcher = patron.matcher(correo);
+                if (matcher.matches()) {
+                    String numeroStr = matcher.group(1);
+                    int numero = 0;
+                    if (!numeroStr.isEmpty()) {
+                        numero = Integer.parseInt(numeroStr);
+                    }
+                    maxNumero = Math.max(maxNumero, numero); //compara los dos números y escoge el más grande
+                }
+            }
+            // Incrementar el número y modificar el correo generado existente
+            nuevoEmail = nombre + "." + apellido + (maxNumero + 1) + "@fasttrack.com." + dominio;
         } else {
             LOGGER.info("El email no existe");
         }
         return nuevoEmail;
     }
 
+    //consulta BD y trae el dominio del pais ingresado
     public String obtenerDominio(String pais) throws SQLException {
         String paiss = "'" + pais + "'";
         String sql = "SELECT * FROM paises WHERE pais =" + paiss;
@@ -69,6 +88,8 @@ public class Email {
         }
         return listaPaises;
     }
+
+    //realiza una lista de todos los emails existentes en BD, será usada en la genración del correo
     public List<String> readEmail(){
         String sql = "SELECT email FROM estudiante";
         List <String> listaEmails = new ArrayList<>();
